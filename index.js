77ade06a95577a956b147335352b6d5ff819e312
Aditya yadav <adityayadaw42@gmail.com>
2025-05-22 11:53:56 +0530

    index route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const path=require('path');
 const Listing=require('./models/listing.js');
 const app=express();
 
 const MONGO_URL="mongodb://localhost:27017/wanderlust";
 const port=8080;
+
+app.set('view engine','ejs');
+app.set("views",path.join(__dirname,"views"));
 
 async function main(){
     await mongoose.connect(MONGO_URL);
@@ -18,31 +22,36 @@ main()
     console.log("DB Connection Failed");
 })
 
-
-
 app.get('/',(req,res)=>{
     res.send("Root working..");
 })
 
-app.get('/listing/test',async(req,res)=>{
-    const list=new Listing({
-        title:"MY Villa",
-        description:"This is the description of villa",
-        price:80000,
-        location:"Rishikesh Uttarakhand",
-        country:"India"
-    })
-    await list.save()
-    .then((result)=>{
-        console.log("List is saved")
-        res.send("Successfully inserted")
-    })
-    .catch((err)=>{
-        console.log("List save failed")
-        res.send("List is not saved");
-    })
+//index Route
 
+app.get('/listings',async(req,res)=>{
+    const allListings= await Listing.find({});
+    res.render('listings/index.ejs',{allListings});
 })
+
+// app.get('/listing/test',async(req,res)=>{
+//     const list=new Listing({
+//         title:"MY Villa",
+//         description:"This is the description of villa",
+//         price:80000,
+//         location:"Rishikesh Uttarakhand",
+//         country:"India"
+//     })
+//     await list.save()
+//     .then((result)=>{
+//         console.log("List is saved")
+//         res.send("Successfully inserted")
+//     })
+//     .catch((err)=>{
+//         console.log("List save failed")
+//         res.send("List is not saved");
+//     })
+
+// })
 
 app.listen(port,()=>{
     console.log(`App is listening on port ${port}`);
